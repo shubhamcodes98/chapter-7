dd09589c91b7d3f4809e1835ef3bf92ad42655ab
shubhamcodes98 <sr3683142@gmail.com>
2026-08-19 23:45:14 +0530

    Add files via upload

---
+++ For loop.py
@@ -0,0 +1,8 @@
+for a in range(0,100,4):
+    print(a)
+
+# step size function bhi use hota hai range mein matlab har use count ki value show karega humne isme 4 stepssize liya hai to  
+# output mein har 4th number show karega jaise 4 and 8 and 12 and 16
+
+
+
